42b53128ea3141a6c3116292106921c79c7f2dd5
WillmanGZ <willmang@uninorte.edu.co>
2025-12-30 09:57:51 -0500

    Add About, Contact, and Pricing pages with basic layout and metadata

---
+++ app/(general)/about/page.tsx
@@ -0,0 +1,10 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Que cule viaje vale",
+  description: "SEO Title",
+};
+
+export default function AboutPage() {
+  return <span className="text-7xl">About Page</span>;
+}
---
+++ app/(general)/contact/page.tsx
@@ -0,0 +1,7 @@
+export default function ContatPage() {
+  return (
+    <>
+      <span>Contact Page</span>
+    </>
+  );
+}
